55e2dac00906dd59635254c04e663f42119a64c1
Usuario <Usuario@DESKTOP-FT7LAG6>
2024-02-25 15:28:01 -0600

    Agregar archivos de proyecto.

---
+++ Exercise 2 U3/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Exercise_2_U3
+{
+    internal class program
+    {
+        public static void Main(string[] args)
+        {
+            extent a = new extent();
+            a . @Extent (5);
+            Console.WriteLine (a.ToString ());
+
+            extent b = new extent ();
+            b . @Extent (10);
+
+            extent c = new extent ();
+            c.Perimeter();
+            Console.WriteLine (c.ToString ());
+            
+
+        }
+    }
+}
---
+++ Exercise 2 U3/extent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_2_U3
+{
+    internal class extent
+    {
+        public int _Extent;
+
+        public int E
+        {
+            get { return _Extent; }
+            set { _Extent = value; }
+        }
+
+        public extent()
+        {
+            _Extent = 0;
+        }
+
+        public void @Extent (int extent_)
+        {
+            _Extent = extent_;
+        }
+
+        public int Perimeter (int _perimeter)
+        {
+            return _perimeter = _Extent * 12;
+        }
+
+        public int Perimeter ()
+        {
+            return _Extent / 12;
+        }
+
+        ~extent()
+        {
+            //clean
+        }
+    }
+
+}
